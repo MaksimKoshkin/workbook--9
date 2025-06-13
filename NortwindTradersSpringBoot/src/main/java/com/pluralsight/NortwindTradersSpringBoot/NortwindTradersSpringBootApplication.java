8a143696d9ac136cefb469b60d5ad7da35bebe3a
Maksim Koshkin <mkoshkin@my.yearupunited.org>
2025-06-13 14:37:03 -0400

    Start

---
+++ NortwindTradersSpringBoot/src/main/java/com/pluralsight/NortwindTradersSpringBoot/NortwindTradersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NortwindTradersSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NortwindTradersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NortwindTradersSpringBootApplication.class, args);
+	}
+
+}
